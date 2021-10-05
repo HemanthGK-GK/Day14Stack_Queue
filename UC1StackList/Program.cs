01d2d1b71cbe588b7b0b53f73a583c7120a7ee9f
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-05 18:22:21 +0530

    Hemanth:Insert A Integer to Stack using LinkedList

---
+++ UC1StackList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace UC1StackList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stk = new Stack();
+            stk.Push(70);
+            stk.Push(30);
+            stk.Push(56);
+
+            foreach(var i in stk)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ UC1StackList/StkLink.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC1StackList
+{
+    class StkLink
+    {
+        public void push(int value)
+        {
+
+        }
+
+    }
+}
