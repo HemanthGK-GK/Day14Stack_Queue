9af960ab955cdebff374d96a91a3faaccebd2fe5
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-08 16:20:41 +0530

    Hemanth : Removing values from statck using POP

---
+++ StkPop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StkPop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StkLink stk = new StkLink();
+            stk.Push(70);
+            stk.Push(30);
+            stk.Push(56);
+
+            stk.Peek();
+            stk.Pop();
+            stk.ChkEmpty();
+        }
+    }
+}
---
+++ StkPop/StkLink.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StkPop
+{
+    class StkLink
+    {
+        internal class Node
+        {
+            internal int data;
+            internal Node next;
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+        private Node top;
+        public StkLink()
+        {
+            this.top = null;
+        }
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("Value popped is : "+ this.top.data);
+            this.top = this.top.next;
+        }
+
+        public void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("Top of the stack : "+this.top.data);
+        }
+        public void ChkEmpty()
+        {
+            while (this.top != null)
+            {
+                Pop();
+            }
+            
+        }
+    }
+}
