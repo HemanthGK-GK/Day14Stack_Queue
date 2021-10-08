fbd6e4694e5fed936a075d5f024ca9cb44c79b0c
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-08 16:24:07 +0530

    Hemanth : Inserting values to the Queue

---
+++ UC3QueueInsert/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UC3QueueInsert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueueLink link = new QueueLink();
+            link.Enqueue(56);
+            link.Enqueue(30);
+            link.Enqueue(70);
+        }
+    }
+}
---
+++ UC3QueueInsert/QueueLink.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC3QueueInsert
+{
+   public class QueueLink
+    {
+        public class Node
+        {
+            public int data;
+            public Node next;
+            public Node(int data)
+            {
+                this.data = data;
+            }
+        }
+        Node head = null;
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+                head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+                
+            }
+            Console.WriteLine(node.data+" inserted into queue " );
+        }
+    }
+}
