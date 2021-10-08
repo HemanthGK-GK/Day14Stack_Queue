7be04c271be12d709973900672f203887fce0b0f
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-08 16:26:35 +0530

    Hemanth : Removing values from Queue using Dequeue Method

---
+++ UC4Dequeue/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UC4Dequeue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueueLink link = new QueueLink();
+            link.Enqueue(56);
+            link.Enqueue(30);
+            link.Enqueue(70);
+            link.Print();
+            link.Dequeue();
+            link.Dequeue();
+            link.Print();
+
+        }
+    }
+}
